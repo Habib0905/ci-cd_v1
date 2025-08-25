69379ce22e6fb44dea65dc15becfe0a4dde13ef2
Habib Hussain <hussainhabib05@yahoo.com>
2025-08-25 14:09:28 +0600

    add server.js file

---
+++ app/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: '../.env' });
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (_req, res) => {
+res.send(`Hello from Node.js via Docker + NGINX! Time: ${new Date().toISOString()}`);
+});
+
+app.listen(port, () => console.log(`App listening on ${port}`));
